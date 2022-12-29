574121a063a49aa57b1efaf963124798cb5dd70c
Jose Avalos <jsuava@gmail.com>
2022-12-28 22:08:52 -0500

    Cambiando conexiones para depurar

--- src/main/java/com/cystesoft/vyrbus/service/util/Constantes.java
+++ src/main/java/com/cystesoft/vyrbus/service/util/Constantes.java
@@ -158,14 +158,10 @@ public class Constantes {
 //	public static final String SERVER_HOST = Executions.getCurrent().getHeader("host");
 //	public static final String SERVER_HOST = "ventas.tepsa.com.pe/";
 
-	//http://168.121.236.46:8080/
-
 //	public static final String SERVER_HOST = "localhost:8080";
 
 	//SERVER_HOST PRODUCCION TRANSMAR
 	public static final String SERVER_HOST = "119.8.145.122:8080";
-//	public static final String SERVER_HOST = "190.119.140.203:8080";
-
 
 	public static final String SERVER_CONTEXT = Executions.getCurrent().getContextPath();
 	public static final String URL_FORMATOS = SERVER_PROTOCOL+"://"+SERVER_HOST+SERVER_CONTEXT+"/formatos/";
