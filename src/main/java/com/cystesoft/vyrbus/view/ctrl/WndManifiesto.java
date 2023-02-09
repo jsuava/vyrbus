9cea5158053fadae64ebc2dc96b5757b743526f6
Mark Oscco <osccoem@gmail.com>
2023-02-08 22:33:31 -0500

    Cuadre de Manifiesto

--- src/main/java/com/cystesoft/vyrbus/view/ctrl/WndManifiesto.java
+++ src/main/java/com/cystesoft/vyrbus/view/ctrl/WndManifiesto.java
@@ -89,7 +89,7 @@ import sun.misc.BASE64Encoder;
 
 /**
  *
- * @author Josï¿½ Abanto.
+ * @author José Abanto.
  */
 public class WndManifiesto extends WndBase {
 	private static final long serialVersionUID = -3533022967415262311L;
@@ -248,7 +248,7 @@ public class WndManifiesto extends WndBase {
 		cargarTipoImpresionManifiesto();
 //		//recupera la agencia asociada al usuario hardware
 //		lstUsuarioHardware= UtilData.agenciaUsuarioHardware(UtilData.getCodigoUsuarioHardware());
-		/*recupera datos autorizados por la sunat para la emisiï¿½n del manifiesto.*/
+		/*recupera datos autorizados por la sunat para la emisión del manifiesto.*/
 		loadCorrelativosManifiesto();
 
 		cmdImprimir.setDisabled(true);
@@ -268,7 +268,7 @@ public class WndManifiesto extends WndBase {
 	public void GrabaManifiesto(final boolean isPrintLaser) throws Exception{
 		ArrayList<VentaPasaje>listCompPendientesXImprimir = new ArrayList<>();
 		try{
-			/*recupera datos autorizados por la sunat para la emisiï¿½n del manifiesto.*/
+			/*recupera datos autorizados por la sunat para la emisión del manifiesto.*/
 			loadCorrelativosManifiesto();
 			/*Correlativos para el Manifiesto Agotados*/
 			if (porcentajeCorrelativoManifiesto>=100)
@@ -423,11 +423,11 @@ public class WndManifiesto extends WndBase {
 	}
 
 	/**
-	 * Recupera datos autorizados por la sunat para la emisiï¿½n del manifiesto.
+	 * Recupera datos autorizados por la sunat para la emisión del manifiesto.
 	 * @throws Exception
 	 */
 	private void loadCorrelativosManifiesto() throws Exception{
-//		/*recupera datos autorizado por la Sunat, segï¿½n la agencia*/
+//		/*recupera datos autorizado por la Sunat, según la agencia*/
 //		//especieValoradaSunat = new EspecieValorada();
 //		if (lstUsuarioHardware.size() >0){
 //			Agencia agencia =  new Agencia();
@@ -784,9 +784,9 @@ public class WndManifiesto extends WndBase {
 	}
 
 	/**
-	 * Permite enlazar los controles a la ventana de selecciï¿½n de Itinerario
+	 * Permite enlazar los controles a la ventana de selección de Itinerario
 	 * @param textboxItinerario :en este Textbox se devolvera el Id del itinerario seleccionado.
-	 * @param button :ha este Button se le adjuntara un listener con la llamada a la ventana de selecciï¿½n de itinerario
+	 * @param button :ha este Button se le adjuntara un listener con la llamada a la ventana de selección de itinerario
 	 * @see WndItinerario:
 	 */
 	public  void enlazarItinerario(final Button button) {
@@ -869,7 +869,7 @@ public class WndManifiesto extends WndBase {
 				cmdImprimir.setDisabled(true);
 				cmdPrevio.setDisabled(true);}
 		}else{
-			/*Habili/Desabilita segï¿½n la configuracion del rol del usuario*/
+			/*Habili/Desabilita según la configuracion del rol del usuario*/
 			/*IMPRIMIR*/
 			cmdImprimir.setDisabled(accesoImprimir()?false:true);
 
@@ -992,7 +992,7 @@ public class WndManifiesto extends WndBase {
 				
 				
 				//************************************************************************************
-				//Consulta la version de impresiï¿½n configurada para la agencia - jabanto 16/11/2022
+				//Consulta la version de impresión configurada para la agencia - jabanto 16/11/2022
 				Agencia oagencia = (Agencia)Executions.getCurrent().getSession().getAttribute(Constantes.ATRIBUTO_AGENCIA);
 				
 				if(UtilFlag.isFormatPrintViewPdfCarpetaDespacho(oagencia.getId())) {
@@ -1149,7 +1149,7 @@ public class WndManifiesto extends WndBase {
 					
 										
 					//************************************************************************************
-					//Consulta la version de impresiï¿½n configurada para la agencia - jabanto 16/11/2022
+					//Consulta la version de impresión configurada para la agencia - jabanto 16/11/2022
 					Agencia oagencia = (Agencia)Executions.getCurrent().getSession().getAttribute(Constantes.ATRIBUTO_AGENCIA);
 					
 					if(UtilFlag.isFormatPrintViewPdfManifiesto(oagencia.getId())) {
@@ -1856,7 +1856,7 @@ public class WndManifiesto extends WndBase {
 		final String simboloNull="-----";
 
 		/*Etiqueta Agencia venta*/
-		label= new Label("Agencia creaciï¿½n :");
+		label= new Label("Agencia creación :");
 		label.setStyle("font-size:11px !important");
 		row.appendChild(label);
 		/*Valor Agencia venta*/
@@ -1876,7 +1876,7 @@ public class WndManifiesto extends WndBase {
 
 		/*Etiqueta Usuario venta*/
 		row= new Row();
-		label= new Label("Usuario creaciï¿½n :");
+		label= new Label("Usuario creación :");
 		label.setStyle("font-size:11px !important");
 		row.appendChild(label);
 		/*Valor Usuario venta*/
@@ -1896,7 +1896,7 @@ public class WndManifiesto extends WndBase {
 
 		/*Fecha/Hora Creacion*/
 		row=new Row();row.setSpans("1,4");
-		label= new Label("Fecha/Hora creaciï¿½n :");
+		label= new Label("Fecha/Hora creación :");
 		label.setStyle("font-size:11px !important");
 		row.appendChild(label);
 		rows.appendChild(row);
@@ -1918,7 +1918,7 @@ public class WndManifiesto extends WndBase {
 
 		/*Usuario Modificacion*/
 		row=new Row();
-		label= new Label("Usuario Modificaciï¿½n :");
+		label= new Label("Usuario Modificación :");
 		label.setStyle("font-size:11px !important");
 		row.appendChild(label);
 		rows.appendChild(row);
@@ -1927,7 +1927,7 @@ public class WndManifiesto extends WndBase {
 		row.appendChild(lblUsuarioModificacion);
 
 		/*Fecha y hora Modificacion*/
-		label= new Label("Fecha/Hora Modificaciï¿½n :");
+		label= new Label("Fecha/Hora Modificación :");
 		label.setStyle("font-size:11px !important");
 		row.appendChild(label);
 		rows.appendChild(row);
@@ -1949,7 +1949,7 @@ public class WndManifiesto extends WndBase {
 				lblAgenciaRemota.setValue(simboloNull);
 				lblUsuarioRmoto.setValue(simboloNull);
 
-				// Valida que el nï¿½mero del asiento ingrasado no se mayor a la capasidad del bus.
+				// Valida que el número del asiento ingrasado no se mayor a la capasidad del bus.
 
 
 				if(!(txtItinerario.getText().trim().isEmpty())){
