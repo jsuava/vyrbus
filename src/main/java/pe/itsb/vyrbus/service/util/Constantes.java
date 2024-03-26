875ef2257d6db334c8a5a7378cde6db91490e327
Mark Oscco <osccoem@gmail.com>
2024-03-26 15:05:26 -0500

    Cadenas de conexion y version

--- src/main/java/pe/itsb/vyrbus/service/util/Constantes.java
+++ src/main/java/pe/itsb/vyrbus/service/util/Constantes.java
@@ -26,7 +26,7 @@ public class Constantes {
 	 * Vension actual del Sistema
 	 */
 
-	public static final String SYSTEM_VERSION = "Versión 3.4.3";
+	public static final String SYSTEM_VERSION = "Versión 3.4.4";
 
 	/**
 	 * Constante para la Accion Nuevo
