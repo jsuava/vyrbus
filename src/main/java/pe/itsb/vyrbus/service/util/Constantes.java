cfa0dea16b49acb3bd527638aa2aa5e8af3d2094
Mark Oscco <osccoem@gmail.com>
2024-07-15 18:27:47 -0500

    Version y constantes

--- src/main/java/pe/itsb/vyrbus/service/util/Constantes.java
+++ src/main/java/pe/itsb/vyrbus/service/util/Constantes.java
@@ -26,7 +26,7 @@ public class Constantes {
 	 * Vension actual del Sistema
 	 */
 
-	public static final String SYSTEM_VERSION = "Versión 3.4.9";
+	public static final String SYSTEM_VERSION = "Versión 3.5.0";
 
 	/**
 	 * Constante para la Accion Nuevo
